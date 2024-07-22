cb7fb767a3dedf4e2a0b1d213bfbff0cc06e72f7
nnijgnus <76596376+k000927@users.noreply.github.com>
2024-07-22 17:16:07 +0900

    init
    init

---
+++ CasperEvent/src/test/java/JGS/CasperEvent/CasperEventApplicationTests.java
@@ -0,0 +1,13 @@
+package JGS.CasperEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasperEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
