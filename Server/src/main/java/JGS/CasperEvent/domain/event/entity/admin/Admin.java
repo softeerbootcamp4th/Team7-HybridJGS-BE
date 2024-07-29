610c19c7e1209a1e726a120d8f027fe7f22f108f
k000927 <kimsungjin927@gmail.com>
2024-07-29 15:38:41 +0900

    chore: Admin 엔티티 작성

---
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/admin/Admin.java
@@ -0,0 +1,8 @@
+package JGS.CasperEvent.domain.event.entity.admin;
+
+import JGS.CasperEvent.global.entity.BaseEntity;
+
+public class Admin extends BaseEntity {
+    private String adminId;
+    private String password;
+}
