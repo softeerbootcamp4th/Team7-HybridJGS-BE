7dd8993d1fc2aeffb60b22ca73bfaa38cc34c387
k000927 <kimsungjin927@gmail.com>
2024-07-30 17:36:24 +0900

    chore: 유저 번호 반환하는 더미메서드 추가

---
+++ Server/src/main/java/JGS/CasperEvent/global/util/UserUtil.java
@@ -0,0 +1,9 @@
+package JGS.CasperEvent.global.util;
+
+public class UserUtil {
+
+    //TODO: 현재는 그냥 userData 즉시 반환, 키 이용한 복호화로 수정하기
+    public static String getDecodedPhoneNumber(String userData){
+        return userData;
+    }
+}
