588cda42ba32c6993223a0442074488f72555a45
k000927 <kimsungjin927@gmail.com>
2024-07-30 17:35:25 +0900

    chore: createdAt 작동 위해 @EnableJpaAuditing 추가

--- Server/src/main/java/JGS/CasperEvent/CasperEventApplication.java
+++ Server/src/main/java/JGS/CasperEvent/CasperEventApplication.java
@@ -2,9 +2,10 @@ package JGS.CasperEvent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaAuditing
 public class CasperEventApplication {
 
 	public static void main(String[] args) {
