0ac709766a18d4b0223e3be816b0ca54fb59a899
k000927 <kimsungjin927@gmail.com>
2024-07-30 15:07:32 +0900

    feat: LotteryEventService 추가

--- Server/src/main/java/JGS/CasperEvent/domain/event/service/eventService/LotteryEventService.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/service/eventService/LotteryEventService.java
@@ -1,4 +1,20 @@
 package JGS.CasperEvent.domain.event.service.eventService;
 
+import JGS.CasperEvent.domain.event.dto.GetCasperBot;
+import JGS.CasperEvent.domain.event.entity.casperBot.CasperBot;
+import JGS.CasperEvent.domain.event.repository.eventRepository.LotteryEventRepository;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Service;
+
+import static JGS.CasperEvent.global.util.GsonUtil.getGson;
+
+@Service
 public class LotteryEventService {
+    private LotteryEventRepository lotteryEventRepository;
+
+    public GetCasperBot postCasperBot(String body) {
+        Gson gson = getGson();
+        CasperBot casperBot = gson.fromJson(body, CasperBot.class);
+        return GetCasperBot.of(casperBot);
+    }
 }
