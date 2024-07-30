86b47074251f9651d82926b9d95d3939b3027f81
k000927 <kimsungjin927@gmail.com>
2024-07-30 17:36:03 +0900

    chore: lotteryEvent와 LotteryParticipants 사이의 연관관계 제거

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
@@ -1,18 +1,12 @@
 package JGS.CasperEvent.domain.event.entity.event;
 
-import JGS.CasperEvent.domain.event.entity.participants.LotteryParticipants;
 import jakarta.persistence.*;
-
-import java.util.Set;
 
 @Entity
 public class LotteryEvent extends BaseEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long lotteryEventId;
-
-    @OneToMany(mappedBy = "lotteryEvent", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<LotteryParticipants> lotteryParticipants;
 
     public Long getLotteryEventId() {
         return lotteryEventId;
--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/LotteryParticipants.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/LotteryParticipants.java
@@ -1,21 +1,24 @@
 package JGS.CasperEvent.domain.event.entity.participants;
 
-import JGS.CasperEvent.domain.event.entity.event.LotteryEvent;
 import JGS.CasperEvent.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 @Entity
 public class LotteryParticipants extends BaseEntity {
     private int linkClickedCount;
     private int expectations;
 
-    @ManyToOne
-    @JoinColumn(name="lottery_event_id")
-    private LotteryEvent lotteryEvent;
-
     @Id
     private String phoneNumber;
+
+    public LotteryParticipants() {
+
+    }
+
+    public LotteryParticipants(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+        this.linkClickedCount = 0;
+        this.expectations = 0;
+    }
 }
