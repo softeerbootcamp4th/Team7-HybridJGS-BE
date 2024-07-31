55eb9e688fa217eb17418a36eeb1fd0d85ba4d9b
k000927 <kimsungjin927@gmail.com>
2024-07-31 18:21:12 +0900

    feat: BaseEntity, LotteryParticipants에 Getter 메서드 추가

--- Server/src/main/java/JGS/CasperEvent/global/entity/BaseEntity.java
+++ Server/src/main/java/JGS/CasperEvent/global/entity/BaseEntity.java
@@ -20,4 +20,12 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
