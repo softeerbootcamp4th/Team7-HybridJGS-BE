71b9e1b1ffc046e41662c71b4db510784e620062
k000927 <kimsungjin927@gmail.com>
2024-07-31 15:14:10 +0900

    chore: 사소한 수정..

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/CasperBotRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/CasperBotRepository.java
@@ -1,7 +1,9 @@
 package JGS.CasperEvent.domain.event.repository;
 
+import JGS.CasperEvent.domain.event.entity.casperBot.CasperBot;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CasperBotRepository {
+public interface CasperBotRepository extends JpaRepository<CasperBot, Long> {
 }
