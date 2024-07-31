16d3803950cd1db819fa67fcbe3cf9b72f823dd6
k000927 <kimsungjin927@gmail.com>
2024-07-31 18:21:24 +0900

    feat: GetLotteryParticipant DTO 추가

---
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/GetLotteryParticipant.java
@@ -0,0 +1,18 @@
+package JGS.CasperEvent.domain.event.dto;
+import JGS.CasperEvent.domain.event.entity.participants.LotteryParticipants;
+
+import java.time.LocalDateTime;
+
+
+public record GetLotteryParticipant(int linkClickedCount, int expectations, int appliedCount,
+                                    LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public static GetLotteryParticipant of(LotteryParticipants lotteryParticipants) {
+        return new GetLotteryParticipant(
+                lotteryParticipants.getLinkClickedCount(),
+                lotteryParticipants.getExpectations(),
+                lotteryParticipants.getAppliedCount(),
+                lotteryParticipants.getCreatedAt(),
+                lotteryParticipants.getUpdatedAt()
+        );
+    }
+}
