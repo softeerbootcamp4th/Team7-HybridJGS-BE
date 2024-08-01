60195a84f8edaffe73dd88dcaf192d1f48efd38f
wjddn2165 <qowjddn2165@gmail.com>
2024-08-01 18:51:22 +0900

    feat: RushEventRepository JPA 설정

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/eventRepository/RushEventRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/eventRepository/RushEventRepository.java
@@ -1,7 +1,9 @@
 package JGS.CasperEvent.domain.event.repository.eventRepository;
 
+import JGS.CasperEvent.domain.event.entity.event.RushEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RushEventRepository {
+public interface RushEventRepository extends JpaRepository<RushEvent, Long> {
 }
