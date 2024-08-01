910400b8e3274dcdd5893e6b805b6ce3eadfd3b2
wjddn2165 <qowjddn2165@gmail.com>
2024-08-01 18:50:05 +0900

    feat: 전체 선착순 이벤트와 서버 시간을 반환하는 DTO RushEventListAndServerTimeResponse 구현

---
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/RushEventListAndServerTimeResponse.java
@@ -0,0 +1,31 @@
+package JGS.CasperEvent.domain.event.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class RushEventListAndServerTimeResponse {
+    private List<GetRushEvent> events;
+    private LocalDateTime serverTime;
+
+    public RushEventListAndServerTimeResponse(List<GetRushEvent> events, LocalDateTime serverTime) {
+        this.events = events;
+        this.serverTime = serverTime;
+    }
+
+    // Getters and setters
+    public List<GetRushEvent> getEvents() {
+        return events;
+    }
+    public void setEvents(List<GetRushEvent> events) {
+        this.events = events;
+    }
+
+    public LocalDateTime getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(LocalDateTime serverTime) {
+        this.serverTime = serverTime;
+    }
+
+}
