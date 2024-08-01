14e37c84397fa66261141d7cb142b610677fa5a5
k000927 <kimsungjin927@gmail.com>
2024-08-01 17:45:43 +0900

    feat: 기본키 생성 메서드 작성

--- Server/src/main/java/JGS/CasperEvent/global/util/UserUtil.java
+++ Server/src/main/java/JGS/CasperEvent/global/util/UserUtil.java
@@ -1,9 +1,17 @@
 package JGS.CasperEvent.global.util;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class UserUtil {
+    private static final AtomicLong counter = new AtomicLong(0);
 
     //TODO: 현재는 그냥 userData 즉시 반환, 키 이용한 복호화로 수정하기
-    public static String getDecodedPhoneNumber(String userData){
+    public static String getDecodedPhoneNumber(String userData) {
         return userData;
     }
+
+    public static long generateId(){
+        return counter.incrementAndGet();
+    }
+
 }
