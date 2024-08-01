9ef1807715071f818d89c6cea3764389f20f3f35
wjddn2165 <qowjddn2165@gmail.com>
2024-08-01 18:49:24 +0900

    feat: 전체 선착순 이벤트 조회 API 구현

--- Server/src/main/java/JGS/CasperEvent/domain/event/controller/eventController/RushEventController.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/controller/eventController/RushEventController.java
@@ -1,11 +1,31 @@
 package JGS.CasperEvent.domain.event.controller.eventController;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import JGS.CasperEvent.domain.event.dto.RushEventListAndServerTimeResponse;
+import JGS.CasperEvent.domain.event.service.eventService.RushEventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/event/rush")
 public class RushEventController {
+    private final RushEventService rushEventService;
 
+    public RushEventController(RushEventService rushEventService) {
+        this.rushEventService = rushEventService;
+    }
+
+    // 전체 선착순 이벤트 조회
+    @GetMapping
+    public ResponseEntity<RushEventListAndServerTimeResponse> getRushEventListAndServerTime() {
+        return ResponseEntity.ok(rushEventService.getAllRushEvents());
+    }
+
+    // 밸런스 게임 참여 여부 조회
+//    @GetMapping("/{eventId}/applied")
+//    public ResponseEntity<Boolean> checkUserParticipationInRushEvent(@PathVariable Long eventId,
+//                                                                     @CookieValue String userData) {
+//
+//        return ResponseEntity.ok(rushEventService.isExists(eventId, userData));
+//    }
 }
