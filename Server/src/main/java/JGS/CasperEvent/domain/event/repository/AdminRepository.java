837a392bd1a6921c041a05dafc69225d82f75545
k000927 <kimsungjin927@gmail.com>
2024-08-05 14:39:32 +0900

    chore: AdminRepository 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/AdminRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/AdminRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, String> {
 }
