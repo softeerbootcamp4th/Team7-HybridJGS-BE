83224f6c6fe81963fb261a49a8ae493ce87ad94a
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:05:08 +0900

    feat: Admin 엔티티 추가

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/admin/Admin.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/admin/Admin.java
@@ -1,12 +1,18 @@
 package JGS.CasperEvent.domain.event.entity.admin;
 
 import JGS.CasperEvent.global.entity.BaseEntity;
+import JGS.CasperEvent.global.entity.BaseUser;
+import JGS.CasperEvent.global.enums.Role;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 @Entity
-public class Admin extends BaseEntity {
-    @Id
-    private String adminId;
+public class Admin extends BaseUser {
     private String password;
+
+    public Admin(String id, String password, Role role) {
+        super(id, role);
+        this.password = password;
+    }
+
+    public Admin() {}
 }
