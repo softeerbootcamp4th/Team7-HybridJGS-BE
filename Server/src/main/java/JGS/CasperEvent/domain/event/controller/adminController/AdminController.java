ea6cbb6f62919922030928b1010a0e5837f444fc
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:05:23 +0900

    feat: Admin 회원가입 API 구현

--- Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
@@ -1,4 +1,25 @@
 package JGS.CasperEvent.domain.event.controller.adminController;
 
+import JGS.CasperEvent.domain.event.service.AdminService.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<String> postAdmin(@RequestBody String body){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(adminService.postAdmin(body));
+    }
 }
