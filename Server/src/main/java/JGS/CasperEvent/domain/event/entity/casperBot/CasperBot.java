adaa9984a695e90339bf9951d265da0c9c0c85ec
k000927 <kimsungjin927@gmail.com>
2024-08-06 15:07:57 +0900

    chore: 사용하지 않는 코드 삭제

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/casperBot/CasperBot.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/casperBot/CasperBot.java
@@ -2,7 +2,6 @@ package JGS.CasperEvent.domain.event.entity.casperBot;
 
 import JGS.CasperEvent.domain.event.dto.RequestDto.PostCasperBot;
 import JGS.CasperEvent.global.entity.BaseEntity;
-import JGS.CasperEvent.global.error.exception.CustomException;
 import JGS.CasperEvent.global.util.UserUtil;
 import jakarta.persistence.*;
 
@@ -71,24 +70,6 @@ public class CasperBot extends BaseEntity {
 
     public String getExpectation() {
         return expectation;
-    }
-
-    public void validateEnumFields() throws CustomException {
-//        if (eyeShape == null) {
-//            throw new CustomException("eyeShape cannot be null", CustomErrorCode.INVALID_CASPERBOT_PARAMETER);
-//        }
-//        if (eyePosition == null) {
-//            throw new CustomException("EyePosition cannot be null", CustomErrorCode.INVALID_CASPERBOT_PARAMETER);
-//        }
-//        if (mouthShape == null) {
-//            throw new CustomException("MouthShape cannot be null", CustomErrorCode.INVALID_CASPERBOT_PARAMETER);
-//        }
-//        if (color == null) {
-//            throw new CustomException("Color cannot be null", CustomErrorCode.INVALID_CASPERBOT_PARAMETER);
-//        }
-//        if (sticker == null) {
-//            throw new CustomException("Sticker cannot be null", CustomErrorCode.INVALID_CASPERBOT_PARAMETER);
-//        }
     }
 
     @Override
