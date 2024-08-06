2e82a2e04e5d540eabd2d2e5921636ac69e49109
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:27:28 +0900

    feat: 유저가 처음 로그인 시도할 때 유저가 이미 존재하는지 확인하는 filter 추가

---
+++ Server/src/main/java/JGS/CasperEvent/global/jwt/filter/VerifyUserFilter.java
@@ -0,0 +1,42 @@
+package JGS.CasperEvent.global.jwt.filter;
+
+import JGS.CasperEvent.global.entity.BaseUser;
+import JGS.CasperEvent.global.jwt.dto.UserLoginDto;
+import JGS.CasperEvent.global.jwt.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class VerifyUserFilter implements Filter {
+    public static final String AUTHENTICATE_USER = "authenticateUser";
+    private final ObjectMapper objectMapper;
+
+    private final UserService userService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if ((httpServletRequest.getMethod().equals("POST"))) {
+            try {
+                UserLoginDto userLoginDto = objectMapper.readValue(request.getReader(), UserLoginDto.class);
+                BaseUser user = userService.verifyUser(userLoginDto);
+
+                request.setAttribute(AUTHENTICATE_USER, user);
+
+                chain.doFilter(request, response);
+            } catch (Exception e) {
+                log.error("Fail User Verify");
+                HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+                httpServletResponse.sendError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+            }
+        }
+    }
+}
