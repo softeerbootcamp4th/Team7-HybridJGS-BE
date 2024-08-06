0c3ae5a2b5b6dc0ffc20c09849667341eaaa1cd4
k000927 <kimsungjin927@gmail.com>
2024-08-06 17:40:55 +0900

    test: MethodArgumentNotValidException 핸들러 작성

--- Server/src/main/java/JGS/CasperEvent/global/error/GlobalExceptionHandler.java
+++ Server/src/main/java/JGS/CasperEvent/global/error/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import JGS.CasperEvent.global.enums.CustomErrorCode;
 import JGS.CasperEvent.global.error.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -33,4 +36,29 @@ public class GlobalExceptionHandler  {
                 .status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponse.of(CustomErrorCode.USER_NOT_FOUND));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e){
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(CustomErrorCode.INVALID_CASPERBOT_PARAMETER, builder.toString()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> RuntimeExceptionHandler(RuntimeException e){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(CustomErrorCode.BAD_REQUEST, e.getMessage()));
+    }
+
 }
