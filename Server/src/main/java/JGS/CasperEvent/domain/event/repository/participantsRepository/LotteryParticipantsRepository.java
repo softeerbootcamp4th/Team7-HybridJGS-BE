0132d265390a37f332d5b81ca45147593375bd89
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:20:48 +0900

    feat: findByPhoneNumber 메서드 삭제

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/participantsRepository/LotteryParticipantsRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/participantsRepository/LotteryParticipantsRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface LotteryParticipantsRepository extends JpaRepository<LotteryParticipants, String> {
-    Optional<LotteryParticipants> findByPhoneNumber(String phoneNumber);
 }
 
