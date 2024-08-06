23683472849f98f13595bde834ee2825c67ee693
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:06:21 +0900

    feat: Admin 회원가입 비즈니스 로직 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/service/AdminService/AdminService.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/service/AdminService/AdminService.java
@@ -1,4 +1,40 @@
 package JGS.CasperEvent.domain.event.service.AdminService;
 
+import JGS.CasperEvent.domain.event.entity.admin.Admin;
+import JGS.CasperEvent.domain.event.repository.AdminRepository;
+import JGS.CasperEvent.global.enums.Role;
+import JGS.CasperEvent.global.error.exception.CustomException;
+import JGS.CasperEvent.global.error.exception.ErrorCode;
+import JGS.CasperEvent.global.jwt.dto.AdminLoginDto;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+@Service
 public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public Admin verifyAdmin(AdminLoginDto adminLoginDto) {
+        return adminRepository.findById(adminLoginDto.getId()).orElseThrow(NoSuchElementException::new);
+    }
+
+    public String postAdmin(String body) {
+        JsonParser jsonParser = new JsonParser();
+
+        JsonObject adminObject = (JsonObject) jsonParser.parse(body);
+
+        String adminId = adminObject.get("id").getAsString();
+        String password = adminObject.get("password").getAsString();
+
+        Admin admin = adminRepository.findById(adminId).orElse(null);
+
+        if (admin != null) throw new CustomException("이미 등록된 ID입니다.", ErrorCode.ACCESS_DENIED);
+        adminRepository.save(new Admin(adminId, password, Role.ADMIN));
+
+        return "admin Created";
+    }
 }
