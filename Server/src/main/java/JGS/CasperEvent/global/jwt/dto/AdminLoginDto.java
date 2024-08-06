b1c0fde91020fe4f6bb28ef3c12ea3600b47a96d
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:18:42 +0900

    feat: Admin 로그인 정보를 담는 AdminLoginDto 구현

--- Server/src/main/java/JGS/CasperEvent/global/jwt/dto/AdminLoginDto.java
+++ Server/src/main/java/JGS/CasperEvent/global/jwt/dto/AdminLoginDto.java
@@ -3,7 +3,7 @@ package JGS.CasperEvent.global.jwt.dto;
 import lombok.Getter;
 
 @Getter
-public class AdminLoginDto implements LoginDto {
+public class AdminLoginDto {
     private String id;
     private String password;
 }
