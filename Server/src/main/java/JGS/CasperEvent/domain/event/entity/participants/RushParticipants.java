82a89319f4e76a23ef013fb20399f9e44c01ccaf
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 11:22:17 +0900

    feat: 선착순 이벤트 참여자가 BaseUser 클래스를 상속받도록 수정

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/RushParticipants.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/RushParticipants.java
@@ -1,20 +1,24 @@
 package JGS.CasperEvent.domain.event.entity.participants;
 
 import JGS.CasperEvent.domain.event.entity.event.RushEvent;
-import JGS.CasperEvent.global.entity.BaseEntity;
+import JGS.CasperEvent.global.entity.BaseUser;
+import JGS.CasperEvent.global.enums.Role;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class RushParticipants extends BaseEntity {
+public class RushParticipants extends BaseUser {
     private int choice;
-
     @ManyToOne
     @JoinColumn(name = "rush_event_id")
     private RushEvent rushEvent;
 
-    @Id
-    private String phoneNumber;
+    public RushParticipants(String id, Role role) {
+        super(id, role);
+    }
+
+    public RushParticipants() {
+        super();
+    }
 }
