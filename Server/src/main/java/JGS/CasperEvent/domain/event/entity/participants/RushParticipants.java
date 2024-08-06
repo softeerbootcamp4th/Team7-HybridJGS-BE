94c485ab77d188e10b7a8fac89811c1920ed69e2
wjddn2165 <qowjddn2165@gmail.com>
2024-08-06 23:27:39 +0900

    fix: RushParticipants 가 BaseUser 상속받지 않도록 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/RushParticipants.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/participants/RushParticipants.java
@@ -2,23 +2,29 @@ package JGS.CasperEvent.domain.event.entity.participants;
 
 import JGS.CasperEvent.domain.event.entity.event.RushEvent;
 import JGS.CasperEvent.global.entity.BaseUser;
-import JGS.CasperEvent.global.enums.Role;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
-public class RushParticipants extends BaseUser {
-    private int choice;
+public class RushParticipants {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int optionId;
+    @OneToOne
+    @JoinColumn(name = "base_user_id")
+    private BaseUser baseUser;
+
     @ManyToOne
     @JoinColumn(name = "rush_event_id")
     private RushEvent rushEvent;
 
-    public RushParticipants(String id, Role role) {
-        super(id, role);
+    public RushParticipants(BaseUser baseUser, RushEvent rushEvent, int optionId) {
+        this.baseUser = baseUser;
+        this.rushEvent = rushEvent;
+        this.optionId = optionId;
     }
 
     public RushParticipants() {
-        super();
+
     }
 }
