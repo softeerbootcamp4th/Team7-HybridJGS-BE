8e076f09efe526323e59964d6eaa3212708faf3f
k000927 <kimsungjin927@gmail.com>
2024-08-07 18:09:30 +0900

    feat: findByIdAndPassword 메서드 추가

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/AdminRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/AdminRepository.java
@@ -4,6 +4,9 @@ import JGS.CasperEvent.domain.event.entity.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
+    Optional<Admin> findByIdAndPassword(String adminId, String password);
 }
