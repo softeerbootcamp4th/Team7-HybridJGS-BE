ee5c83f73ab223c7f75395b906a00ed00cbcd267
k000927 <kimsungjin927@gmail.com>
2024-08-07 16:32:51 +0900

    chore: 클라이언트 테스트용 가짜 API 추가

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
@@ -1,14 +1,12 @@
 package JGS.CasperEvent.domain.event.entity.event;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class LotteryEvent extends BaseEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long lotteryEventId;
-
-    public Long getLotteryEventId() {
-        return lotteryEventId;
-    }
 }
