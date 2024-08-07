2524a00651fcfeb9ac2690ce0b33ca798f99e6d2
k000927 <kimsungjin927@gmail.com>
2024-08-07 18:55:35 +0900

    fix: ddl-auto: update로 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/LotteryParticipantResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/LotteryParticipantResponseDto.java
@@ -6,12 +6,12 @@ import java.time.LocalDateTime;
 
 public record LotteryParticipantResponseDto(int linkClickedCount, int expectations, int appliedCount, CasperBotResponseDto casperBot,
                                             LocalDateTime createdAt, LocalDateTime updatedAt) {
-    public static LotteryParticipantResponseDto of(LotteryParticipants lotteryParticipants, CasperBotResponseDto getcasperBotResponseDto) {
+    public static LotteryParticipantResponseDto of(LotteryParticipants lotteryParticipants, CasperBotResponseDto casperBotResponseDto) {
         return new LotteryParticipantResponseDto(
                 lotteryParticipants.getLinkClickedCount(),
                 lotteryParticipants.getExpectations(),
                 lotteryParticipants.getAppliedCount(),
-                getcasperBotResponseDto,
+                casperBotResponseDto,
                 lotteryParticipants.getBaseUser().getCreatedAt(),
                 lotteryParticipants.getBaseUser().getUpdatedAt()
         );
