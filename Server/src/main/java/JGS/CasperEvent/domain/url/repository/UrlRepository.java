228f024cda56c2c33400a6653b23dc7800cea46a
nnijgnus <kimsungjin927@gmail.com>
2024-08-10 02:09:51 +0900

    test: 단축 URL 생성 메서드 작성

--- Server/src/main/java/JGS/CasperEvent/domain/url/repository/UrlRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/url/repository/UrlRepository.java
@@ -1,9 +1,12 @@
 package JGS.CasperEvent.domain.url.repository;
 
-import JGS.CasperEvent.domain.url.entity.OriginalUrl;
+import JGS.CasperEvent.domain.url.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UrlRepository extends JpaRepository<OriginalUrl, Long> {
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByOriginalUrl(String originalUrl);
 }
