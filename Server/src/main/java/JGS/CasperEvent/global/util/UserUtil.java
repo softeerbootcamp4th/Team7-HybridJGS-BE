c9666a2c1b094a891244d259f9a5427d44eddaf0
nnijgnus <kimsungjin927@gmail.com>
2024-08-10 02:13:24 +0900

    chore: 사용하지 않는 메서드 및 클래스 삭제

--- Server/src/main/java/JGS/CasperEvent/global/util/UserUtil.java
+++ Server/src/main/java/JGS/CasperEvent/global/util/UserUtil.java
@@ -5,17 +5,6 @@ import java.util.concurrent.atomic.AtomicLong;
 public class UserUtil {
     //TODO: 스프링 서버 뻗으면 캐스퍼 아이디 0부터 다시 시작함
     private static final AtomicLong counter = new AtomicLong(0);
-
-    //TODO: 현재는 그냥 userData 즉시 반환, 키 이용한 복호화로 수정하기
-    public static String getDecodedPhoneNumber(String userData) {
-        return userData;
-    }
-
-    //TODO: 현재는 true 리턴, jwt로 변경 필요
-    public static Boolean isValidAdminToken(String token){
-        if (token.equals("adminToken")) return true;
-        return false;
-    }
     public static long generateId(){
         return counter.incrementAndGet();
     }
