52cfd8c51bbabaeca0045a483151334a09475012
nnijgnus <kimsungjin927@gmail.com>
2024-08-09 11:13:34 +0900

    chore: UrlRepository 작성

--- Server/src/main/java/JGS/CasperEvent/domain/url/repository/UrlRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/url/repository/UrlRepository.java
@@ -1,4 +1,9 @@
 package JGS.CasperEvent.domain.url.repository;
 
-public interface UrlRepository {
+import JGS.CasperEvent.domain.url.entity.OriginalUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<OriginalUrl, Long> {
 }
