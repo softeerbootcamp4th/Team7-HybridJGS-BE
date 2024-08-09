838ab36889f5fda5f8eedd890ca84ee2de8a7fd8
nnijgnus <kimsungjin927@gmail.com>
2024-08-09 11:13:07 +0900

    fix: GeneratedValue strategy Identity로 변경

--- Server/src/main/java/JGS/CasperEvent/domain/url/entity/OriginalUrl.java
+++ Server/src/main/java/JGS/CasperEvent/domain/url/entity/OriginalUrl.java
@@ -1,13 +1,15 @@
 package JGS.CasperEvent.domain.url.entity;
 
 import JGS.CasperEvent.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class OriginalUrl extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String originalUrl;
