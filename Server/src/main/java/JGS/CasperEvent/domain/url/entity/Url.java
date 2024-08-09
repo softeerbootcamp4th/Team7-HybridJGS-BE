d795e1cdcdb2533e31914b7fe05ae88aa9ce9ba4
nnijgnus <kimsungjin927@gmail.com>
2024-08-10 02:06:36 +0900

    chore: URL 엔티티 이름 변경 및 생성자 추가

--- Server/src/main/java/JGS/CasperEvent/domain/url/entity/Url.java
+++ Server/src/main/java/JGS/CasperEvent/domain/url/entity/Url.java
@@ -5,12 +5,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
-public class OriginalUrl extends BaseEntity {
+@Getter
+public class Url extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String originalUrl;
+
+    public Url(String originalUrl){
+        this.originalUrl = originalUrl;
+    }
+
+    public Url() {
+
+    }
 }
