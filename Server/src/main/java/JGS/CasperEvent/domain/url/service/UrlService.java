b63919ee31dcdcf8c9e72c51bc9d748726526187
nnijgnus <kimsungjin927@gmail.com>
2024-08-09 11:14:34 +0900

    chore: UrlService 작성

--- Server/src/main/java/JGS/CasperEvent/domain/url/service/UrlService.java
+++ Server/src/main/java/JGS/CasperEvent/domain/url/service/UrlService.java
@@ -1,4 +1,26 @@
 package JGS.CasperEvent.domain.url.service;
 
+import JGS.CasperEvent.domain.url.dto.ShortenUrlResponseDto;
+import JGS.CasperEvent.domain.url.repository.UrlRepository;
+import JGS.CasperEvent.global.entity.BaseUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UrlService {
+
+    private final UrlRepository urlRepository;
+
+    @Autowired
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    public ShortenUrlResponseDto generateShortUrl(BaseUser user){
+        String id = user.getId();
+
+        System.out.println("id = " + id);
+
+        return new ShortenUrlResponseDto("shortenLink");
+    }
 }
