f89d14f52e3968c2f2a959f29c21940c9f6ad675
wjddn2165 <qowjddn2165@gmail.com>
2024-08-11 14:05:15 +0900

    feat: LocalDateTime 직렬화 가능하도록 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/BaseEvent.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/BaseEvent.java
@@ -1,23 +1,38 @@
 package JGS.CasperEvent.domain.event.entity.event;
 
 import JGS.CasperEvent.global.entity.BaseEntity;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+@Getter
+@MappedSuperclass
 public class BaseEvent extends BaseEntity {
-    protected LocalDate eventStartDate;
-    protected LocalDate eventEndDate;
-    protected int winnerCount;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime startDateTime;
 
-    public LocalDate getEventStartDate() {
-        return eventStartDate;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime endDateTime;
+    private int winnerCount;
+
+    // 기본 생성자에서 디폴트 값 설정
+    public BaseEvent() {
+        this.startDateTime = LocalDateTime.now();
+        this.endDateTime = LocalDateTime.now().plusMinutes(10);
+        this.winnerCount = 0; // 기본 우승자 수를 0으로 설정
     }
 
-    public LocalDate getEventEndDate() {
-        return eventEndDate;
-    }
-
-    public int getWinnerCount() {
-        return winnerCount;
+    // 특정 값을 설정할 수 있는 생성자
+    public BaseEvent(LocalDateTime startDateTime, LocalDateTime endDateTime, int winnerCount) {
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.winnerCount = winnerCount;
     }
 }
