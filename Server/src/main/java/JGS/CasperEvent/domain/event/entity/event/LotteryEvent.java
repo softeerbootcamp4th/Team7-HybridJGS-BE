fd0fd2e18bbdb1e1ef6a2ba3b7db6e9f1970c872
nnijgnus <kimsungjin927@gmail.com>
2024-08-11 14:09:18 +0900

    feat: LotteryEvent 생성자 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/entity/event/LotteryEvent.java
@@ -3,10 +3,22 @@ package JGS.CasperEvent.domain.event.entity.event;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 public class LotteryEvent extends BaseEvent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long lotteryEventId;
+
+    public LotteryEvent(LocalDateTime eventStartDate, LocalDateTime eventEndDate, int winnerCount){
+        this.eventStartDate = eventStartDate;
+        this.eventEndDate = eventEndDate;
+        this.winnerCount = winnerCount;
+    }
+
+    public LotteryEvent() {
+
+    }
 }
