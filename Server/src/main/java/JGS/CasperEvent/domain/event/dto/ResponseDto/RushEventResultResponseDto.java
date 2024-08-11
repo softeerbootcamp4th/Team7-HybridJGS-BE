d68c9a5a116d56ad208eddafc6175235b8ed23a7
wjddn2165 <qowjddn2165@gmail.com>
2024-08-11 14:18:12 +0900

    fix: 생성자에서 RushEventRateResponseDto를 받도록 수정

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/RushEventResultResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/RushEventResultResponseDto.java
@@ -5,6 +5,18 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public record RushEventResultResponseDto(long leftOption, long rightOption, long rank, long totalParticipants,
-                                         long winnerCount) {
+public class RushEventResultResponseDto {
+    private final long leftOption;
+    private final long rightOption;
+    private final long rank;
+    private final long totalParticipants;
+    private final long winnerCount;
+
+    public RushEventResultResponseDto(RushEventRateResponseDto rushEventRateResponseDto, long rank, long totalParticipants, long winnerCount) {
+        this.leftOption = rushEventRateResponseDto.leftOption();
+        this.rightOption = rushEventRateResponseDto.rightOption();
+        this.rank = rank;
+        this.totalParticipants = totalParticipants;
+        this.winnerCount = winnerCount;
+    }
 }
