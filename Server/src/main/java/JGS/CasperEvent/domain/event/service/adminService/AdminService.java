b4830139be72c4e62153396b4fc2da595df562ea
nnijgnus <kimsungjin927@gmail.com>
2024-08-13 01:24:44 +0900

    feat: RushEvent 생성 메서드 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/service/adminService/AdminService.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/service/adminService/AdminService.java
@@ -2,26 +2,37 @@ package JGS.CasperEvent.domain.event.service.adminService;
 
 import JGS.CasperEvent.domain.event.dto.RequestDto.AdminRequestDto;
 import JGS.CasperEvent.domain.event.dto.RequestDto.lotteryEventDto.LotteryEventRequestDto;
+import JGS.CasperEvent.domain.event.dto.RequestDto.rushEventDto.RushEventOptionRequestDto;
+import JGS.CasperEvent.domain.event.dto.RequestDto.rushEventDto.RushEventRequestDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventDetailResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventParticipantsListResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventParticipantsResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventResponseDto;
+import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.RushEventResponseDto;
 import JGS.CasperEvent.domain.event.entity.admin.Admin;
 import JGS.CasperEvent.domain.event.entity.event.LotteryEvent;
+import JGS.CasperEvent.domain.event.entity.event.RushEvent;
+import JGS.CasperEvent.domain.event.entity.event.RushOption;
 import JGS.CasperEvent.domain.event.entity.participants.LotteryParticipants;
 import JGS.CasperEvent.domain.event.repository.AdminRepository;
 import JGS.CasperEvent.domain.event.repository.eventRepository.LotteryEventRepository;
+import JGS.CasperEvent.domain.event.repository.eventRepository.RushEventRepository;
+import JGS.CasperEvent.domain.event.repository.eventRepository.RushOptionRepository;
 import JGS.CasperEvent.domain.event.repository.participantsRepository.LotteryParticipantsRepository;
 import JGS.CasperEvent.global.enums.CustomErrorCode;
+import JGS.CasperEvent.global.enums.Position;
 import JGS.CasperEvent.global.enums.Role;
 import JGS.CasperEvent.global.error.exception.CustomException;
 import JGS.CasperEvent.global.error.exception.TooManyLotteryEventException;
+import JGS.CasperEvent.global.error.exception.TooManyRushEventException;
 import JGS.CasperEvent.global.response.ResponseDto;
+import JGS.CasperEvent.global.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -34,7 +45,10 @@ public class AdminService {
 
     private final AdminRepository adminRepository;
     private final LotteryEventRepository lotteryEventRepository;
+    private final RushEventRepository rushEventRepository;
     private final LotteryParticipantsRepository lotteryParticipantsRepository;
+    private final RushOptionRepository rushOptionRepository;
+    private final S3Service s3Service;
 
     public Admin verifyAdmin(AdminRequestDto adminRequestDto) {
         return adminRepository.findById(adminRequestDto.getAdminId()).orElseThrow(NoSuchElementException::new);
@@ -78,9 +92,6 @@ public class AdminService {
         if (phoneNumber.isEmpty()) lotteryParticipantsPage = lotteryParticipantsRepository.findAll(pageable);
         else lotteryParticipantsRepository.findByBaseUser_Id(phoneNumber, pageable);
 
-        for (LotteryParticipants participants : lotteryParticipantsPage) {
-            System.out.println("participants = " + participants);
-        }
         List<LotteryEventParticipantsResponseDto> lotteryEventParticipantsResponseDtoList = new ArrayList<>();
 
         for (LotteryParticipants lotteryParticipant : lotteryParticipantsPage) {
@@ -90,5 +101,50 @@ public class AdminService {
         }
         Boolean isLastPage = !lotteryParticipantsPage.hasNext();
         return new LotteryEventParticipantsListResponseDto(lotteryEventParticipantsResponseDtoList, isLastPage, lotteryParticipantsRepository.count());
+    }
+
+    public RushEventResponseDto createRushEvent(RushEventRequestDto rushEventRequestDto, MultipartFile prizeImg, MultipartFile leftOptionImg, MultipartFile rightOptionImg) {
+        if (rushEventRepository.count() >= 6) throw new TooManyRushEventException();
+
+        String prizeImgSrc = s3Service.upload(prizeImg);
+        String leftOptionImgSrc = s3Service.upload(leftOptionImg);
+        String rightOptionImgSrc = s3Service.upload(rightOptionImg);
+
+        // Img s3 저장
+        RushEvent rushEvent = rushEventRepository.save(
+                new RushEvent(
+                        LocalDateTime.of(rushEventRequestDto.getEventDate(), rushEventRequestDto.getStartTime()),
+                        LocalDateTime.of(rushEventRequestDto.getEventDate(), rushEventRequestDto.getEndTime()),
+                        rushEventRequestDto.getWinnerCount(),
+                        prizeImgSrc,
+                        rushEventRequestDto.getPrizeDescription()
+                ));
+
+        RushEventOptionRequestDto leftOption = rushEventRequestDto.getLeftOption();
+        RushEventOptionRequestDto rightOption = rushEventRequestDto.getRightOption();
+
+        RushOption leftRushOption = rushOptionRepository.save(new RushOption(
+                rushEvent,
+                leftOption.getMainText(),
+                leftOption.getSubText(),
+                leftOption.getResultMainText(),
+                leftOption.getResultSubText(),
+                leftOptionImgSrc,
+                Position.LEFT
+        ));
+
+        RushOption rightRushOption = rushOptionRepository.save(new RushOption(
+                rushEvent,
+                rightOption.getMainText(),
+                rightOption.getSubText(),
+                rightOption.getResultMainText(),
+                rightOption.getResultSubText(),
+                rightOptionImgSrc,
+                Position.RIGHT
+        ));
+
+        rushEvent.updateOption(leftRushOption, rightRushOption);
+
+        return RushEventResponseDto.of(rushEvent);
     }
 }
