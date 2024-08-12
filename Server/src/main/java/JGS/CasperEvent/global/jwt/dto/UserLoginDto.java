6d29479ef0497b47d50e4d5e90f7919982b69885
wjddn2165 <qowjddn2165@gmail.com>
2024-08-12 23:36:44 +0900

    feat: UserLoginDto 생성자 추가

--- Server/src/main/java/JGS/CasperEvent/global/jwt/dto/UserLoginDto.java
+++ Server/src/main/java/JGS/CasperEvent/global/jwt/dto/UserLoginDto.java
@@ -1,8 +1,14 @@
 package JGS.CasperEvent.global.jwt.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UserLoginDto {
     String phoneNumber;
+
+    public UserLoginDto(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
