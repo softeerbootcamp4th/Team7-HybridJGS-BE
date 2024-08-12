8596ee2fdff4bda3c6522717ce56842d23d81b20
nnijgnus <kimsungjin927@gmail.com>
2024-08-12 13:51:40 +0900

    feat: 전체 참가자 조회 API 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
@@ -4,6 +4,7 @@ import JGS.CasperEvent.domain.event.dto.RequestDto.AdminRequestDto;
 import JGS.CasperEvent.domain.event.dto.RequestDto.LotteryEventRequestDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventDetailResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventResponseDto;
+import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.ParticipantsListResponseDto;
 import JGS.CasperEvent.domain.event.service.adminService.AdminService;
 import JGS.CasperEvent.global.response.ResponseDto;
 import jakarta.validation.Valid;
@@ -24,6 +25,7 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    // 어드민 생성
     @PostMapping("/join")
     public ResponseEntity<ResponseDto> postAdmin(@RequestBody @Valid AdminRequestDto adminRequestDto) {
         return ResponseEntity
@@ -31,6 +33,7 @@ public class AdminController {
                 .body(adminService.postAdmin(adminRequestDto));
     }
 
+    // 추첨 이벤트 조회
     @GetMapping("/event/lottery")
     public ResponseEntity<List<LotteryEventDetailResponseDto>> getLotteryEvent() {
         return ResponseEntity
@@ -38,12 +41,23 @@ public class AdminController {
                 .body(adminService.getLotteryEvent());
     }
 
+    // 추첨 이벤트 생성
     @PostMapping("/event/lottery")
     public ResponseEntity<LotteryEventResponseDto> createLotteryEvent(
-            @Valid @RequestBody  LotteryEventRequestDto lotteryEventRequestDto) {
+            @Valid @RequestBody LotteryEventRequestDto lotteryEventRequestDto) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(adminService.createLotteryEvent(lotteryEventRequestDto));
     }
 
+    // 추첨 이벤트 참여자 조회
+    @GetMapping("/event/lottery/participants")
+    public ResponseEntity<ParticipantsListResponseDto> getLotteryEventParticipants(
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "number", required = false) String phoneNumber) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(adminService.getLotteryEventParticipants(size, page, phoneNumber));
+    }
 }
