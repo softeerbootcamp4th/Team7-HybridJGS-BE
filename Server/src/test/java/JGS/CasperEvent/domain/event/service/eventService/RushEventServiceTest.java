cdd9ded2e3a5eb5f24c5f4c492ce26b6b945b15d
wjddn2165 <qowjddn2165@gmail.com>
2024-08-12 23:37:17 +0900

    feat: RushEventServiceTest 선착순 이벤트 서비스 레이어 테스트 껍데기만 작성

---
+++ Server/src/test/java/JGS/CasperEvent/domain/event/service/eventService/RushEventServiceTest.java
@@ -0,0 +1,44 @@
+package JGS.CasperEvent.domain.event.service.eventService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushEventServiceTest {
+
+    @Test
+    void getAllRushEvents() {
+    }
+
+    @Test
+    void isExists() {
+    }
+
+    @Test
+    void apply() {
+    }
+
+    @Test
+    void getRushEventRate() {
+    }
+
+    @Test
+    void getRushEventResult() {
+    }
+
+    @Test
+    void getTodayRushEvent() {
+    }
+
+    @Test
+    void setTodayEventToRedis() {
+    }
+
+    @Test
+    void getTodayRushEventOptions() {
+    }
+
+    @Test
+    void getRushEventOptionResult() {
+    }
+}
