2ac8b3696635a5deb92ced59b0b166db6d49be80
nnijgnus <kimsungjin927@gmail.com>
2024-08-12 14:46:33 +0900

    feat: 참여자 조회 로직 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/service/adminService/AdminService.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/service/adminService/AdminService.java
@@ -3,23 +3,33 @@ package JGS.CasperEvent.domain.event.service.adminService;
 import JGS.CasperEvent.domain.event.dto.RequestDto.AdminRequestDto;
 import JGS.CasperEvent.domain.event.dto.RequestDto.LotteryEventRequestDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventDetailResponseDto;
+import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventParticipantsListResponseDto;
+import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventParticipantsResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.LotteryEventResponseDto;
 import JGS.CasperEvent.domain.event.entity.admin.Admin;
 import JGS.CasperEvent.domain.event.entity.event.LotteryEvent;
+import JGS.CasperEvent.domain.event.entity.participants.LotteryParticipants;
 import JGS.CasperEvent.domain.event.repository.AdminRepository;
 import JGS.CasperEvent.domain.event.repository.eventRepository.LotteryEventRepository;
 import JGS.CasperEvent.domain.event.repository.eventRepository.RushEventRepository;
+import JGS.CasperEvent.domain.event.repository.participantsRepository.LotteryParticipantsRepository;
 import JGS.CasperEvent.global.enums.CustomErrorCode;
 import JGS.CasperEvent.global.enums.Role;
 import JGS.CasperEvent.global.error.exception.CustomException;
+import JGS.CasperEvent.global.error.exception.LotteryEventNotExists;
 import JGS.CasperEvent.global.error.exception.TooManyLotteryEventException;
 import JGS.CasperEvent.global.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -28,6 +38,7 @@ public class AdminService {
     private final AdminRepository adminRepository;
     private final LotteryEventRepository lotteryEventRepository;
     private final RushEventRepository rushEventRepository;
+    private final LotteryParticipantsRepository lotteryParticipantsRepository;
 
     public Admin verifyAdmin(AdminRequestDto adminRequestDto) {
         return adminRepository.findById(adminRequestDto.getAdminId()).orElseThrow(NoSuchElementException::new);
@@ -47,7 +58,7 @@ public class AdminService {
     }
 
     public LotteryEventResponseDto createLotteryEvent(LotteryEventRequestDto lotteryEventRequestDto) {
-        if(lotteryEventRepository.count() >= 1) throw new TooManyLotteryEventException();
+        if (lotteryEventRepository.count() >= 1) throw new TooManyLotteryEventException();
 
         LotteryEvent lotteryEvent = lotteryEventRepository.save(new LotteryEvent(
                 lotteryEventRequestDto.getEventStartDateTime(),
@@ -62,5 +73,26 @@ public class AdminService {
         return LotteryEventDetailResponseDto.of(
                 lotteryEventRepository.findAll()
         );
+    }
+
+    public LotteryEventParticipantsListResponseDto getLotteryEventParticipants(int size, int page, String phoneNumber) {
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<LotteryParticipants> lotteryParticipantsPage = null;
+        if (phoneNumber.isEmpty()) lotteryParticipantsPage = lotteryParticipantsRepository.findAll(pageable);
+        else lotteryParticipantsRepository.findByBaseUser_Id(phoneNumber, pageable);
+
+        for (LotteryParticipants participants : lotteryParticipantsPage) {
+            System.out.println("participants = " + participants);
+        }
+        List<LotteryEventParticipantsResponseDto> lotteryEventParticipantsResponseDtoList = new ArrayList<>();
+
+        for (LotteryParticipants lotteryParticipant : lotteryParticipantsPage) {
+            lotteryEventParticipantsResponseDtoList.add(
+                    LotteryEventParticipantsResponseDto.of(lotteryParticipant)
+            );
+        }
+        Boolean isLastPage = !lotteryParticipantsPage.hasNext();
+        return new LotteryEventParticipantsListResponseDto(lotteryEventParticipantsResponseDtoList, isLastPage, lotteryParticipantsRepository.count());
     }
 }
