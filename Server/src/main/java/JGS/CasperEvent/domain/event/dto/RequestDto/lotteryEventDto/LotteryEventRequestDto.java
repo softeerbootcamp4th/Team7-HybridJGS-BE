8b5afa3827967f6144a88359ae22c5b6729ea0bb
wjddn2165 <qowjddn2165@gmail.com>
2024-08-13 18:31:45 +0900

    fix: LotteryEventRequestDto 에서 날짜와 시간을 분리하도록 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/lotteryEventDto/LotteryEventRequestDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/lotteryEventDto/LotteryEventRequestDto.java
@@ -3,16 +3,23 @@ package JGS.CasperEvent.domain.event.dto.RequestDto.lotteryEventDto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 public class LotteryEventRequestDto {
 
-    @NotNull(message = "이벤트 시작 일자를 지정하세요.")
-    private LocalDateTime eventStartDateTime;
+    @NotNull(message = "이벤트 시작 날짜를 지정하세요.")
+    private LocalDate eventStartDate;
 
-    @NotNull(message = "이벤트 종료 일자를 지정하세요.")
-    private LocalDateTime eventEndDateTime;
+    @NotNull(message = "이벤트 시작 시간을 지정하세요.")
+    private LocalTime eventStartTime;
+
+    @NotNull(message = "이벤트 종료 날짜를 지정하세요.")
+    private LocalDate eventEndDate;
+
+    @NotNull(message = "이벤트 시작 시간을 지정하세요.")
+    private LocalTime eventEndTime;
 
     @NotNull(message = "당첨인원 수를 지정하세요.")
     private int winnerCount;
