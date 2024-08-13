1f570ce4ae24749bab207452a0be344823e9fbdd
nnijgnus <kimsungjin927@gmail.com>
2024-08-13 10:58:37 +0900

    feat: 로컬 테스트용 S3config 분리하여 작성

---
+++ Server/src/main/java/JGS/CasperEvent/global/config/LocalS3Config.java
@@ -0,0 +1,35 @@
+package JGS.CasperEvent.global.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class LocalS3Config {
+    @Value("${spring.cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+}
