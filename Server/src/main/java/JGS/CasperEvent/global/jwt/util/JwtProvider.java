213ad22624bf1f1aa58170a31daff58eba08cbce
nnijgnus <76596376+k000927@users.noreply.github.com>
2024-08-14 18:52:41 +0900

    Update JwtProvider.java

--- Server/src/main/java/JGS/CasperEvent/global/jwt/util/JwtProvider.java
+++ Server/src/main/java/JGS/CasperEvent/global/jwt/util/JwtProvider.java
@@ -40,7 +40,7 @@ public class JwtProvider {
 
 
     private Date getExpireDateAccessToken() {
-        long expireTimeMils = 1000L * 60 * 5;
+        long expireTimeMils = 1000L * 60 * 60  * 24 * 365;
         return new Date(System.currentTimeMillis() + expireTimeMils);
     }
 
