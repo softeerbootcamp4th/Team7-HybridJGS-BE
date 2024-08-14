d67ddd725199068f5de172eb82a33e659d209f31
nnijgnus <kimsungjin927@gmail.com>
2024-08-15 02:31:55 +0900

    feat: 당첨자 저장하기 위한 리포지토리 작성

---
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/participantsRepository/LotteryWinnerRepository.java
@@ -0,0 +1,10 @@
+package JGS.CasperEvent.domain.event.repository.participantsRepository;
+
+import JGS.CasperEvent.domain.event.entity.participants.LotteryParticipants;
+import JGS.CasperEvent.domain.event.entity.participants.LotteryWinners;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LotteryWinnerRepository extends JpaRepository<LotteryWinners, Long> {
+}
