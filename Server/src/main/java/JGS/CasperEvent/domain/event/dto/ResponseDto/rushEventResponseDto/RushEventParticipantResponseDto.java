796607d67ac8c3d7107e0499b911c4c408407ad1
nnijgnus <kimsungjin927@gmail.com>
2024-08-14 17:55:32 +0900

    feat: 선착순 이벤트 참여자 조회 반환 타입 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventParticipantResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventParticipantResponseDto.java
@@ -2,18 +2,20 @@ package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
 import JGS.CasperEvent.domain.event.entity.participants.RushParticipants;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public record RushEventParticipantResponseDto(Long id, String phoneNumber,
-                                              int balanceGameChoice, LocalDateTime createdAt, LocalDateTime updatedAt,
+                                              int balanceGameChoice, LocalDate createdDate, LocalTime createTime,
                                               Long rank) {
     public static RushEventParticipantResponseDto of(RushParticipants rushParticipants, Long rank){
         return new RushEventParticipantResponseDto(
                 rushParticipants.getId(),
                 rushParticipants.getBaseUser().getId(),
                 rushParticipants.getOptionId(),
-                rushParticipants.getCreatedAt(),
-                rushParticipants.getUpdatedAt(),
+                rushParticipants.getCreatedAt().toLocalDate(),
+                rushParticipants.getCreatedAt().toLocalTime(),
                 rank
         );
     }
