e35860ad5701173fd492fb04d97d235675cf1f56
nnijgnus <kimsungjin927@gmail.com>
2024-08-15 16:05:35 +0900

    feat: 페이지네이션 적용한 조회 메서드 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/repository/participantsRepository/LotteryWinnerRepository.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/repository/participantsRepository/LotteryWinnerRepository.java
@@ -1,9 +1,12 @@
 package JGS.CasperEvent.domain.event.repository.participantsRepository;
 
 import JGS.CasperEvent.domain.event.entity.participants.LotteryWinners;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LotteryWinnerRepository extends JpaRepository<LotteryWinners, Long> {
+    Page<LotteryWinners> findByPhoneNumber(String phoneNumber, Pageable pageable);
 }
