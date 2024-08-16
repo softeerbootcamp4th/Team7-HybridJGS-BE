0b71ef4c6025155c0cbb4351e35dd1a233f9f736
nnijgnus <kimsungjin927@gmail.com>
2024-08-17 01:14:15 +0900

    chore: EqualsAndHashCode 어노테이션 추가

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/AdminRequestDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/AdminRequestDto.java
@@ -1,9 +1,13 @@
 package JGS.CasperEvent.domain.event.dto.RequestDto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@Builder
+@EqualsAndHashCode
 public class AdminRequestDto {
 
     @NotNull
