33cb8261e105b9f6128c82cac0b355e426c0b619
nnijgnus <kimsungjin927@gmail.com>
2024-08-17 01:14:01 +0900

    chore: 사용하지 않는 import 삭제

--- Server/src/main/java/JGS/CasperEvent/global/config/WebConfig.java
+++ Server/src/main/java/JGS/CasperEvent/global/config/WebConfig.java
@@ -15,7 +15,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
