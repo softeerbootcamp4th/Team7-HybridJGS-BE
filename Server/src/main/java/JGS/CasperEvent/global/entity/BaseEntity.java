956d94cd058fb0af82a3cde7629d82182f5263d2
nnijgnus <kimsungjin927@gmail.com>
2024-08-18 23:36:54 +0900

    chore: BaseEntity의 Setter 삭제

--- Server/src/main/java/JGS/CasperEvent/global/entity/BaseEntity.java
+++ Server/src/main/java/JGS/CasperEvent/global/entity/BaseEntity.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -19,7 +18,6 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-@Setter
 public class BaseEntity {
     @CreatedDate
     @JsonSerialize(using = LocalDateTimeSerializer.class)
