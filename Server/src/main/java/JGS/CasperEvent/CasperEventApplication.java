e87a2c6b79083316b264eb529f811f06022cb291
wjddn2165 <qowjddn2165@gmail.com>
2024-08-21 00:06:57 +0900

    chore: 메인 애플리케이션에 @EnableCaching 애노테이션 추가

--- Server/src/main/java/JGS/CasperEvent/CasperEventApplication.java
+++ Server/src/main/java/JGS/CasperEvent/CasperEventApplication.java
@@ -2,12 +2,14 @@ package JGS.CasperEvent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
+@EnableCaching
 public class CasperEventApplication {
 
 	public static void main(String[] args) {
