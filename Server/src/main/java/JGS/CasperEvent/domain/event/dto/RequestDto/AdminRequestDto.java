c8f365e2b47ce8e14d863c67336ffd4a131a4d91
nnijgnus <kimsungjin927@gmail.com>
2024-08-21 10:39:50 +0900

    docs: RequestDto에 Swagger 통한 문서화 작업

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/AdminRequestDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/RequestDto/AdminRequestDto.java
@@ -1,17 +1,21 @@
 package JGS.CasperEvent.domain.event.dto.RequestDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Builder
 @EqualsAndHashCode
+@Schema(description = "관리자 계정 생성 요청을 위한 데이터입니다.")
 public class AdminRequestDto {
 
     @NotNull
+    @Schema(description = "관리자 아이디", example = "adminId", required = true)
     private String adminId;
 
     @NotNull
+    @Schema(description = "관리자 인증을 위한 비밀번호", example = "password", required = true)
     private String password;
 
 }
