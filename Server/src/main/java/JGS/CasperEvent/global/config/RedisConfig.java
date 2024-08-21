b13f76de7d2c3ee3f8a07172ba7c37b6fbb7f538
nnijgnus <kimsungjin927@gmail.com>
2024-08-21 16:25:07 +0900

    refactor: unused import문 삭제

--- Server/src/main/java/JGS/CasperEvent/global/config/RedisConfig.java
+++ Server/src/main/java/JGS/CasperEvent/global/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package JGS.CasperEvent.global.config;
 
 import JGS.CasperEvent.domain.event.dto.ResponseDto.lotteryEventResponseDto.CasperBotResponseDto;
-import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.RushEventResponseDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
