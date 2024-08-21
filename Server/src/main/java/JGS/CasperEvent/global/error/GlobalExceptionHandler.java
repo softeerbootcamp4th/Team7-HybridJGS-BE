eef61197f28d37af9736185e79f57dcf6f7bb150
wjddn2165 <qowjddn2165@gmail.com>
2024-08-21 15:20:46 +0900

    fix: RequestInterceptor 에서 afterCompletion 에 예외를 잡아서 log 를 남기던 로직을 GlobalExceptionHandler 로 이동, 또한 필요없는 ExceptionHandler 제거

--- Server/src/main/java/JGS/CasperEvent/global/error/GlobalExceptionHandler.java
+++ Server/src/main/java/JGS/CasperEvent/global/error/GlobalExceptionHandler.java
@@ -2,45 +2,31 @@ package JGS.CasperEvent.global.error;
 
 import JGS.CasperEvent.global.enums.CustomErrorCode;
 import JGS.CasperEvent.global.error.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.nio.file.attribute.UserPrincipalNotFoundException;
-
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler  {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handler(CustomException e){
+        log.error("CustomException [{}]", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.valueOf(e.getErrorCode().getStatus()))
                 .body(ErrorResponse.of(e.getErrorCode(), e.getMessage()));
     }
 
-    @ExceptionHandler(MissingRequestCookieException.class)
-    public ResponseEntity<ErrorResponse> missingCookieHandler(){
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(ErrorResponse.of(CustomErrorCode.UNAUTHORIZED));
-    }
-
-    @ExceptionHandler(UserPrincipalNotFoundException.class)
-    public ResponseEntity<ErrorResponse> userPrincipalNotFoundHandler(){
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(ErrorResponse.of(CustomErrorCode.USER_NOT_FOUND));
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e){
-        BindingResult bindingResult = e.getBindingResult();
+        log.error("MethodArgumentNotValidException [{}]", e.getMessage(), e);
 
+        BindingResult bindingResult = e.getBindingResult();
         StringBuilder builder = new StringBuilder();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
             builder.append(fieldError.getDefaultMessage());
@@ -56,6 +42,7 @@ public class GlobalExceptionHandler  {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> RuntimeExceptionHandler(RuntimeException e){
+        log.error("RuntimeException [{}]", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse.of(CustomErrorCode.BAD_REQUEST, e.getMessage()));
