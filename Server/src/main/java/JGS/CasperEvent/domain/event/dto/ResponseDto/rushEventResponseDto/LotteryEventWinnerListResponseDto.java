f4966a79aaa7df02b8d5781435a0d750b14edee0
nnijgnus <kimsungjin927@gmail.com>
2024-08-23 00:14:56 +0900

    refactor: LotteryEventWinnerListResponseDto 필드 값 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/LotteryEventWinnerListResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/LotteryEventWinnerListResponseDto.java
@@ -1,7 +1,9 @@
 package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
+import JGS.CasperEvent.domain.event.dto.response.lottery.LotteryEventParticipantResponseDto;
+
 import java.util.List;
 
-public record LotteryEventWinnerListResponseDto(List<LotteryEventWinnerResponseDto> participantsList,
+public record LotteryEventWinnerListResponseDto(List<LotteryEventParticipantResponseDto> participantsList,
                                                 Boolean isLastPage, long totalParticipants) {
 }
