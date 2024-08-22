0c591bc0baa43956192a93533f463432a0e76e2f
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 19:03:25 +0900

    chore: RushEventListResponseDto 구조 변경

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventListResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventListResponseDto.java
@@ -1,5 +1,6 @@
 package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
+import JGS.CasperEvent.domain.event.dto.response.rush.RushEventResponseDto;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -8,14 +9,14 @@ import java.util.List;
 
 @Getter
 public class RushEventListResponseDto {
-    private List<MainRushEventResponseDto> events;
+    private List<RushEventResponseDto> events;
     private LocalDateTime serverTime;
     private Long todayEventId;
     private LocalDate eventStartDate;
     private LocalDate eventEndDate;
     private Long activePeriod;
 
-    public RushEventListResponseDto(List<MainRushEventResponseDto> events, LocalDateTime serverTime, Long todayEventId, LocalDate eventStartDate, LocalDate eventEndDate, Long activePeriod) {
+    public RushEventListResponseDto(List<RushEventResponseDto> events, LocalDateTime serverTime, Long todayEventId, LocalDate eventStartDate, LocalDate eventEndDate, Long activePeriod) {
         this.events = events;
         this.serverTime = serverTime;
         this.todayEventId = todayEventId;
