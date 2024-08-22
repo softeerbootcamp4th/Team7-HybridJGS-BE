134c6f19c56a11bb4a7d13dc6c3326a8eb754a4c
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 16:49:34 +0900

    refactor: 선착순 이벤트 선택지 반환하는 of 메서드 적

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/MainRushEventOptionResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/MainRushEventOptionResponseDto.java
@@ -1,12 +1,14 @@
 package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
+import JGS.CasperEvent.domain.event.dto.response.rush.RushEventOptionResponseDto;
+
 public record MainRushEventOptionResponseDto(String mainText,
                                              String subText) {
 
     public static MainRushEventOptionResponseDto of(RushEventOptionResponseDto rushEventOptionResponseDto) {
         return new MainRushEventOptionResponseDto(
-                rushEventOptionResponseDto.mainText(),
-                rushEventOptionResponseDto.subText()
+                rushEventOptionResponseDto.getMainText(),
+                rushEventOptionResponseDto.getSubText()
         );
     }
 }
