f07a23cfa4a209ccf0ba07b9356a9af1bcfaa4d3
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 11:46:46 +0900

    refactor: LotteryEventResponse 교체

--- Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/controller/adminController/AdminController.java
@@ -9,6 +9,7 @@ import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.AdminRu
 import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.AdminRushEventResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.LotteryEventWinnerListResponseDto;
 import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.RushEventParticipantsListResponseDto;
+import JGS.CasperEvent.domain.event.dto.response.LotteryEventResponseDto;
 import JGS.CasperEvent.domain.event.service.adminService.AdminService;
 import JGS.CasperEvent.global.response.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
