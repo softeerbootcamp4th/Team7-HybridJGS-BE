53e74187b2e8c058426e2b989b1da09ca191bdae
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 11:44:01 +0900

    chore: ResponseMessage 적용

--- Server/src/main/java/JGS/CasperEvent/domain/health/api/HealthController.java
+++ Server/src/main/java/JGS/CasperEvent/domain/health/api/HealthController.java
@@ -1,19 +1,22 @@
 package JGS.CasperEvent.domain.health.api;
 
+import JGS.CasperEvent.global.response.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
+@ResponseBody
 public class HealthController {
 
     @GetMapping
-    public ResponseEntity<String> health(){
+    public ResponseEntity<ResponseDto> health(){
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body("Server OK");
+                .body(new ResponseDto("Server OK"));
     }
 }
