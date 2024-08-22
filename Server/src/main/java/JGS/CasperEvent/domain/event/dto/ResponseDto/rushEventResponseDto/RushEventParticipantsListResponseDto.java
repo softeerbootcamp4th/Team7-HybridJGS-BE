e6b2ea3dd8079853169cba62641717bc72f590db
nnijgnus <kimsungjin927@gmail.com>
2024-08-23 00:09:55 +0900

    refactor: RushEventParticipantResponseDto의 result 메서드 적용

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventParticipantsListResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/RushEventParticipantsListResponseDto.java
@@ -1,5 +1,7 @@
 package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
+import JGS.CasperEvent.domain.event.dto.response.rush.RushEventParticipantResponseDto;
+
 import java.util.List;
 public record RushEventParticipantsListResponseDto(List<RushEventParticipantResponseDto> participantsList, Boolean isLastPage, long totalParticipants) {
 }
