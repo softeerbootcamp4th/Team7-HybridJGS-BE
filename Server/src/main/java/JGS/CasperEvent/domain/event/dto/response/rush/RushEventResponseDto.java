595711956c5f2e903b94b4583e0e295becd65045
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 17:23:44 +0900

    feat: 선착순 이벤트의 상세 정보를 조회하는 withDetail 메서드 작성

---
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/response/rush/RushEventResponseDto.java
@@ -0,0 +1,71 @@
+package JGS.CasperEvent.domain.event.dto.response.rush;
+
+import JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto.AdminRushEventResponseDto;
+import JGS.CasperEvent.domain.event.entity.event.RushEvent;
+import JGS.CasperEvent.global.enums.EventStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RushEventResponseDto {
+    private Long rushEventId;
+    private LocalDate eventDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int winnerCount;
+    private String prizeImageUrl;
+    private String prizeDescription;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private EventStatus status;
+    private Set<RushEventOptionResponseDto> options;
+
+    private RushEventResponseDto(Long rushEventId, LocalDate eventDate,
+                                 LocalTime startTime, LocalTime endTime,
+                                 int winnerCount, String prizeImageUrl,
+                                 String prizeDescription, LocalDateTime createdAt,
+                                 LocalDateTime updatedAt, EventStatus status,
+                                 Set<RushEventOptionResponseDto> options) {
+        this.rushEventId = rushEventId;
+        this.eventDate = eventDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.winnerCount = winnerCount;
+        this.prizeImageUrl = prizeImageUrl;
+        this.prizeDescription = prizeDescription;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.status = status;
+        this.options = options;
+    }
+
+    // AdminRushEventResponseDto
+    public static RushEventResponseDto withDetail(RushEvent rushEvent) {
+        Set<RushEventOptionResponseDto> options = rushEvent.getOptions().stream()
+                .map(RushEventOptionResponseDto::of)
+                .collect(Collectors.toSet());
+
+        LocalDateTime now = LocalDateTime.now();
+        EventStatus status;
+        if (now.isBefore(rushEvent.getStartDateTime())) status = EventStatus.BEFORE;
+        else if (now.isAfter(rushEvent.getEndDateTime())) status = EventStatus.AFTER;
+        else status = EventStatus.DURING;
+
+        return new RushEventResponseDto(
+                rushEvent.getRushEventId(),
+                rushEvent.getStartDateTime().toLocalDate(),
+                rushEvent.getStartDateTime().toLocalTime(),
+                rushEvent.getEndDateTime().toLocalTime(),
+                rushEvent.getWinnerCount(),
+                rushEvent.getPrizeImageUrl(),
+                rushEvent.getPrizeDescription(),
+                rushEvent.getCreatedAt(),
+                rushEvent.getUpdatedAt(),
+                status,
+                options
+        );
+    }
+}
