043ced2c8f23f3a92caf6c9fc9ed77d1b51af313
nnijgnus <kimsungjin927@gmail.com>
2024-08-22 17:18:25 +0900

    feat: 결과창 띄울 선택지를 반환하는 inMain 메서드 작성

--- Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/MainRushEventOptionsResponseDto.java
+++ Server/src/main/java/JGS/CasperEvent/domain/event/dto/ResponseDto/rushEventResponseDto/MainRushEventOptionsResponseDto.java
@@ -3,6 +3,7 @@ package JGS.CasperEvent.domain.event.dto.ResponseDto.rushEventResponseDto;
 
 import JGS.CasperEvent.domain.event.dto.response.rush.RushEventOptionResponseDto;
 
+// rush Event로
 public record MainRushEventOptionsResponseDto(RushEventOptionResponseDto leftOption,
                                               RushEventOptionResponseDto rightOption) {
 }
